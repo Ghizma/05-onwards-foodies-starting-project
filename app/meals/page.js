933044b3688660948230b8fa676883f149fafc74
Denis <136696817+Ghizma@users.noreply.github.com>
2026-05-30 16:43:03 +0300

    Handling Errors

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
+export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
@@ -23,7 +26,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={styles.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
       ;
     </>
