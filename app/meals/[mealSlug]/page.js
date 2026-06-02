769251ff3edf12997bfb775ef93bf986571b3efc
Denis <136696817+Ghizma@users.noreply.github.com>
2026-06-02 13:26:17 +0300

    Throwing Not Found Errors For Individual Meals Getting Started with the Share Meal Form

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,13 +1,40 @@
-import Link from "next/link";
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-const MealsDetails = ({ params }) => {
+const MealsDetailsPage = ({ params }) => {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <p>Meals - {params.mealSlug}</p>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 };
 
-export default MealsDetails;
+export default MealsDetailsPage;
