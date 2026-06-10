feef82594ae6f5351217e68818c2cafdd037f68e
Denis <136696817+Ghizma@users.noreply.github.com>
2026-06-10 12:49:57 +0300

    Building For Production & Understanding NextJS Caching Triggering Cache Revalidations

--- app/meals/error.js
+++ app/meals/error.js
@@ -5,7 +5,7 @@ export default function Error({ error }) {
     <>
       <main className={"error"}>
         <h1>An error occured!</h1>
-        <p>Failed to fetch meals data. Please try again later.</p>
+        <p>Failed to create meal.</p>
       </main>
       ;
     </>
